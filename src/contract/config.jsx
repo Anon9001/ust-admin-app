fa9d690bafe7dc7ecb271aab7aa2de7cc8cf20fd
Anon9001 <90354611+Anon9001@users.noreply.github.com>
2022-07-22 19:52:20 -0400

    testnet to main net change
    updated config so we use main net instead of test net.

--- src/contract/config.jsx
+++ src/contract/config.jsx
@@ -1,3 +1,3 @@
-export const CONTRACT_ADDR = "terra1zuy5dmpamma8vaymuta94tyq59npk6sfxn8scwh63tjkwl8ahyjs524g3t";
-export const CHAIN_ID = 1489;
-export const URL = "https://pisco-lcd.terra.dev";
+export const CONTRACT_ADDR = "terra12r6gl4czhye2rejzuawyryaj30wsvnch3qr0ped4aknwzz9lua5qm46xx8";
+export const CHAIN_ID = 205;
+export const URL = "https://phoenix-lcd.terra.dev/";
